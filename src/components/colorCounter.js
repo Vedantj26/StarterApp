4d0dc2084db32c43920d62eaa267dd0286fcf2e5
Vedant Jadhav <vedantjadhav261999@gmail.com>
2023-12-19 09:11:33 +0530

    Made Changes in Square Screen

--- src/components/colorCounter.js
+++ src/components/colorCounter.js
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import { Button, Text, View } from "react-native";
 
-const colorCounter = ({ color }) => {
+const colorCounter = ({ color, onIncrease, onDecrease }) => {
   return (
     <View>
       <Text>{color}</Text>
-      <Button title={`Increase ${color}`} />
-      <Button title={`Decrease ${color}`} />
+      <Button title={`Increase ${color}`} onPress={() => onIncrease()} />
+      <Button title={`Decrease ${color}`} onPress={() => onDecrease()} />
     </View>
   );
 };
